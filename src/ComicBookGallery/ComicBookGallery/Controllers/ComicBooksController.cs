4b227e459a971d4162425d7ae906f5335117eb7a
Reyneer <natsunr@hotmail.com>
2019-04-14 19:45:53 -0600

    updated detail action method

--- src/ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -8,9 +8,13 @@ namespace ComicBookGallery.Controllers
 {
     public class ComicBooksController : Controller
     {
-        public string Detail()
+        public ActionResult Detail()
         {
-            return "Hola mundo"; 
+            if (DateTime.Today.DayOfWeek == DayOfWeek.Monday) {
+                return Redirect("/");
+                //return new RedirectResult("/");
+            }
+            return Content("Hello world");
         }
 
     }
